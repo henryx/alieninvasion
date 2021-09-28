61abc9dec433f2a359a58a987a431b37727b189f
Enrico Bianchi <enrico.bianchi@gmail.com>
2021-09-29 00:00:29 +0200

    Parsed data from file

--- cmd/alieninvasion/main.go
+++ cmd/alieninvasion/main.go
@@ -1,32 +1,49 @@
 package main
 
 import (
+	"alieninvasion/world"
 	"bufio"
 	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
-func parse(filename string) error {
+func parse(filename string) ([]*world.City, error) {
+	var cities []*world.City
 	var err error
 
 	file, err := os.Open(filename)
 	if err != nil {
-		return errors.New(fmt.Sprintf("Error opening file: %v\n", err))
+		return nil, errors.New(fmt.Sprintf("Error opening file: %v\n", err))
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-
+		line := scanner.Text()
+		split := strings.Split(line, " ")
+		city := world.NewCity(split[0])
+		for _, v := range split[1:] {
+			pos := strings.Split(v, "=")
+			near := world.NewCity(pos[1])
+			direction, err := world.GetDirection(pos[0])
+			if err != nil {
+				return nil, err
+			}
+			if err := city.AddNear(near, direction); err != nil {
+				return nil, err
+			}
+		}
+		cities = append(cities, city)
 	}
 
 	if err = scanner.Err(); err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return cities, nil
 }
 
 func main() {
@@ -35,7 +52,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	err := parse(os.Args[1])
+	_, err := parse(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
