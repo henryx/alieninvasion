9a8897cf1192c8bb77f3af96b7e2c290469163f1
Enrico Bianchi <enrico.bianchi@gmail.com>
2021-09-28 23:52:00 +0200

    Added function to get direction

--- world/consts.go
+++ world/consts.go
@@ -1,8 +1,25 @@
 package world
 
+import "errors"
+
 type Point string
 
 const NORTH Point = "north"
 const SOUTH Point = "south"
 const EAST Point = "east"
 const WEST Point = "west"
+
+func GetDirection(direction string) (Point, error) {
+	switch direction {
+	case "north":
+		return NORTH, nil
+	case "south":
+		return SOUTH, nil
+	case "east":
+		return EAST, nil
+	case "west":
+		return WEST, nil
+	default:
+		return "", errors.New("invalid cardinal point")
+	}
+}
