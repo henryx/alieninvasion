2d8fc633b997a9acfd62b38ea5d1dbb12c5e401e
Enrico Bianchi <enrico.bianchi@gmail.com>
2021-09-28 23:58:06 +0200

    Added tests

---
+++ world/consts_test.go
@@ -0,0 +1,19 @@
+package world
+
+import "testing"
+
+func TestGetDirection(t *testing.T) {
+	_, err := GetDirection("west")
+	if err != nil {
+		t.Fatal()
+	}
+}
+
+func TestGetDirection_Fail(t *testing.T) {
+	_, err := GetDirection("foo")
+	if err != nil {
+		return
+	}
+
+	t.Fatal()
+}
