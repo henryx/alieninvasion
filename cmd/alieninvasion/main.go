2b3084c60ddc20ae3204129a00ce78722f039133
Enrico Bianchi <enrico.bianchi@gmail.com>
2021-09-29 23:31:13 +0200

    Added parameter for aliens

--- cmd/alieninvasion/main.go
+++ cmd/alieninvasion/main.go
@@ -6,24 +6,41 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
 	var err error
+	var aliens = 16
 
 	if len(os.Args) < 2 {
-		log.Printf("Usage: %v <worldmap>\n", os.Args[0])
+		log.Printf("Usage: %v <worldmap> [aliens]\n", os.Args[0])
 		os.Exit(1)
+	}
+
+	if len(os.Args) == 3 {
+		aliens, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatal(errors.New(fmt.Sprintf("Aliens need to be a number: %city\n", err)))
+		}
+
+		if aliens == 0 {
+			log.Fatal("Aliens can't be 0")
+		}
 	}
 
 	file, err := os.Open(os.Args[1])
 	if err != nil {
-		log.Fatal(errors.New(fmt.Sprintf("Error opening file: %v\n", err)))
+		log.Fatal(errors.New(fmt.Sprintf("Error opening file: %city\n", err)))
 	}
 	defer file.Close()
 
-	_, err = world.GetCities(file)
+	cities, err := world.GetCities(file)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	if aliens > 2*len(cities) {
+		log.Fatalf("Aliens are greater than the double of cities")
 	}
 }
