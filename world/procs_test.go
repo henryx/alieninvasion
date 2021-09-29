130f992d67a32859d5ef76fa250c74e4b162dfda
Enrico Bianchi <enrico.bianchi@gmail.com>
2021-09-30 00:19:32 +0200

    Added test

--- world/procs_test.go
+++ world/procs_test.go
@@ -25,7 +25,7 @@ func writeFile(file *os.File, str string) error {
 	if err != nil {
 		return err
 	}
-	w.Flush()
+	_ = w.Flush()
 
 	return nil
 }
@@ -71,5 +71,33 @@ func TestGetCitiesFail(t *testing.T) {
 
 	if len(data) > 0 {
 		t.Fatal(err)
+	}
+}
+
+func TestInvade(t *testing.T) {
+	var err error
+
+	file := openFile(t)
+	defer func() {
+		if err := file.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	err = writeFile(file, "Foo north=Bar east=Hou south=Qu-ux west=Baz")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	file.Seek(0, 0)
+	data, err := GetCities(file)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	Invade(&data, len(data)*2)
+
+	if data[0].Aliens == 0 {
+		t.Fatal("Aliens haven't invaded city")
 	}
 }
