840fe08118be8e5f60b4ba418517f747ed10e6d4
Enrico Bianchi <enrico.bianchi@gmail.com>
2021-09-30 23:16:08 +0200

    Added function used to attack cities

--- world/aliens.go
+++ world/aliens.go
@@ -1,6 +1,7 @@
 package world
 
 import (
+	"fmt"
 	"math/rand"
 )
 
@@ -16,6 +17,41 @@ func Invade(cities *map[string]*City, aliens int) {
 			if city.Id == id {
 				city.Aliens++
 				break
+			}
+		}
+	}
+}
+
+func Attack(cities *map[string]*City) {
+	for cityName, city := range *cities {
+		if city.Aliens == 1 {
+			if len(city.Directions) == 0 {
+				fmt.Println("Aliens in city", cityName, "are trapped!")
+				city.Trapped = true
+			}
+
+			if city.Trapped {
+				continue
+			}
+
+			direction := RandDirection()
+			if cityNear, ok := city.Directions[direction]; ok {
+				fmt.Println("An alien from", cityName, "move to", cityNear)
+				(*cities)[cityNear].Aliens++
+				city.Aliens--
+			}
+		}
+
+		if city.Aliens == 2 {
+			fmt.Println("City", cityName, "is destroyed")
+			delete(*cities, cityName)
+			for nearCity, near := range *cities {
+				for direction, c := range near.Directions {
+					if cityName == c {
+						fmt.Println("Delete from", nearCity, "city", cityName, "at", direction)
+						delete(near.Directions, direction)
+					}
+				}
 			}
 		}
 	}
